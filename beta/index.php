ec0a7c6b076263b645131ea696f0d2826ac2f3f8
steezytuff <et556@drexel.edu>
2025-12-04 07:03:02 -0500

    folder change

---
+++ beta/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>PHP Main Menu Dynamic</title>
+  <link rel="stylesheet" href="./styles/general.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="">
+  <html lang="en">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    
+</body>
+
+</html>
